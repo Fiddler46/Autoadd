eba37f45fe0bfc7db0bebed95a42463bfec7501b
Fiddler46 <adhyaksh99@gmail.com>
2021-06-24 13:17:32 +0530

    Installled extra node modules, added auth requests

--- autoadd.js
+++ autoadd.js
@@ -1,3 +1,132 @@
 import Express from "express";
+import request from 'request'; // "Request" library
+import cors from 'cors';
+import querystring from 'querystring';
+import cookieParser from 'cookie-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = Express();
+const port = 3030;
+
+// CLIENT_SECRET stored in Config Vars
+const apiUrl = "https://accounts.spotify.com/api/token"; // Spotify Web API URL
+const client_id = '467fab359c114e719ecefafd6af299e5'; // Client id
+const redirect_uri = 'http://localhost:3030/callback/'; // Callback URL
+
+const scope = [
+    'user-read-private', 
+    'user-read-email',
+    'user-library-read',
+    'playlist-read-private',
+    'playlist-modify-public',
+    'playlist-modify-private'
+];
+
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+ let generateRandomString = function(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+  
+  let stateKey = 'spotify_auth_state';
+  
+  
+  app.use(Express.static(__dirname + '/public'))
+     .use(cors())
+     .use(cookieParser());
+  
+  app.get('/login', function(req, res) {
+  
+    let state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+  //  app requests authorization
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+app.get('/callback', function(req, res) {
+
+    // app requests refresh and access tokens
+    // after checking the state parameter
+  
+    let code = req.query.code || null;
+    let state = req.query.state || null;
+    let storedState = req.cookies ? req.cookies[stateKey] : null;
+  
+    if (state === null || state !== storedState) {
+      res.redirect('/#' +
+        querystring.stringify({
+          error: 'state_mismatch'
+        }));
+    } else {
+      res.clearCookie(stateKey);
+      let authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: redirect_uri,
+          grant_type: 'authorization_code'
+        },
+        headers: {
+          'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        },
+        json: true
+      };
+
+  
+      request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+  
+          let access_token = body.access_token,
+              refresh_token = body.refresh_token;
+  
+          let options = {
+            url: 'https://api.spotify.com/v1/me',
+            headers: { 'Authorization': 'Bearer ' + access_token },
+            json: true
+          };
+            // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+            console.log(body);
+          });
+  
+          // we can also pass the token to the browser to make requests from there
+          res.redirect('/#' +
+            querystring.stringify({
+              access_token: access_token,
+              refresh_token: refresh_token
+            }));
+        } else {
+          res.redirect('/#' +
+            querystring.stringify({
+              error: 'invalid_token'
+            }));
+        }
+      });
+    }
+  });
+  
+  // Write code for app here
+app.listen(port, () => console.log(`Listening on port: ${port}`);
+
+
